c4e27c5f6872cf7e1a0b06819a1497945c026205
Chris Hyer <hyer.chris@gmail.com>
2024-07-10 22:56:56 -0700

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,24 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const OK = Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`);
+const NOT_FOUND = Buffer.from(`HTTP/1.1 404 Not Found\r\n\r\n`);
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   console.log("Client connected");
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-  socket.end();
+  socket.on("data", (data) => {
+    const request = data.toString();
+    console.log(request.split("\r\n"));
+    const [method, path, version] = request.split("\r\n")[0].split(" ");
+    if (path === "/") {
+      socket.write(OK);
+    } else {
+      socket.write(NOT_FOUND);
+    }
+    socket.end();
+  });
+  // socket.end();
 });
 
 server.listen(4221, "localhost");
