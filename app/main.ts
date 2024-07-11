e22966c1b07ddfe22a11df4c5934d88773cf5f01
Chris Hyer <hyer.chris@gmail.com>
2024-07-10 19:28:47 -0700

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,16 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
+    console.log("Client disconnected");
     socket.end();
+  });
+  socket.on("connect", () => {
+    console.log("Client connected");
+  });
+  socket.on("data", (data) => {
+    console.log("Data received from client: ", data.toString());
+    socket.write(Buffer.from(`HTTP/1.1 200 OK \r\n\r\n`));
+    socket.emit("close");
   });
 });
 
